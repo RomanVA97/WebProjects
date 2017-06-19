27ce221906b591f99fa32794c4a9799a886e1a0f
RomanVA97 <vasilje.roman2012@yandex.ru>
2017-06-20 00:23:34 +0300

    Update TiBerisScript.js

--- TiBerisScript.js
+++ TiBerisScript.js
@@ -1,10 +1,11 @@
-var d=document.createElement('div');
-d.style.marginLeft='auto';
+var d=document.createElement('div');//Create div element
+d.style.marginLeft='auto';//Add to div element styles
 d.style.marginRight='auto';
 d.style.border='2px solid black';
 d.style.width='1169px';
 d.style.height='162px';
 
+//Add to div child html elements
 d.innerHTML = "<a href='http:www.yandex.by'>" +
 "	<img src='https://www.przone.ru/images/programs/ximg1207-588.png.pagespeed.ic.jFu73NGc_U.png' width='154' height='115' alt='lorem'>" +
 "</a>"+
