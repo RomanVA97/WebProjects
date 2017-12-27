ab6787d0392810d5ac4b08c13f4fdd23c5baa8d8
RomanVA97 <vasilje.roman2012@yandex.ru>
2017-12-27 16:36:29 +0300

    added task3

---
+++ IBAtasks/task3/task3/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public double Salary { get; set; }
+
+
+        public static List<Employee> SalarySort(List<Employee> list)
+        {
+            return list.OrderByDescending(x => x.Salary).ToList();
+        }
+
+        public static List<Employee> AgeSort(List<Employee> list)
+        {
+            return list.OrderBy(x => x.Age).ToList();
+        }
+
+        public override string ToString()
+        {
+            return "FIO : " + Name + " " + FirstName + " " + LastName + " Age : " + Age + " Salary - " + Salary;
+        }
+
+    }
+}
---
+++ IBAtasks/task3/task3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            List<Employee> list = new List<Employee>();
+            for (int i = 0; i < 5; i++)
+            {
+                Employee employee = new Employee();
+                employee.Name = "name_" + (i + 1);
+                employee.FirstName = "firstName_" + (i + 1);
+                employee.LastName = "lastName_" + (i + 1);
+                employee.Age = random.Next(18, 70);
+                employee.Salary = random.Next(500, 1100);
+                list.Add(employee);
+            }
+
+
+            foreach (Employee item in list)
+                Console.WriteLine(item.ToString());
+            Console.WriteLine("Salary");
+            list = Employee.SalarySort(list);
+            foreach (Employee item in list)
+                Console.WriteLine(item.ToString());
+
+            Console.WriteLine("Age");
+            list = Employee.AgeSort(list);
+            foreach (Employee item in list)
+                Console.WriteLine(item.ToString());
+
+
+            Console.ReadKey();
+        }
+    }
+}
