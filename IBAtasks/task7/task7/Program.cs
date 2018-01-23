40e3c32ad4080f132b4fb41e2436526202898ef4
RomanVA97 <vasilje.roman2012@yandex.ru>
2018-01-23 12:32:41 +0300

    added task7

---
+++ IBAtasks/task7/task7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = new string[4];
+            array[0] = "doG";
+            array[2] = "Cat";
+            array[3] = "bIRd";
+            TextTransformer textTransformer = new TextTransformer(array);
+
+            foreach (string item in textTransformer.GetEnumerator())
+                Console.WriteLine(item);
+
+            foreach (string item in textTransformer.GetTransformStrings())
+                Console.WriteLine(item);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ IBAtasks/task7/task7/TextTransformer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task7
+{
+    class TextTransformer
+    {
+        string[] array;
+
+
+        public TextTransformer(string[] array)
+        {
+            this.array = array;
+        }
+
+
+        public IEnumerable<string> GetTransformStrings()
+        {
+            string str;
+            foreach(string item in array)
+            {
+                if (item == null) str = "something valuable";
+                else
+                {
+                    str = item.ToUpper();
+                }
+
+                yield return str;
+            }
+        }
+
+        public IEnumerable<string> GetEnumerator()
+        {
+            for (int i = 0; i < array.Length; i++)
+                yield return array[i];
+        }
+        
+    }
+}
