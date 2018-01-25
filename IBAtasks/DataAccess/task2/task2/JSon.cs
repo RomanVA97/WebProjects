7242dc8ed7cb6d1476fcdaa4c3f253b46b1a4601
RomanVA97 <vasilje.roman2012@yandex.ru>
2018-01-25 11:43:39 +0300

    added task2

---
+++ IBAtasks/DataAccess/task2/task2/JSon.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.IO;
+
+
+
+namespace task2
+{
+    
+
+    public class JSon
+    {
+
+        
+        public static List<Book> Read(StreamReader str)
+        {
+            JsonTextReader jtr = new JsonTextReader(str);
+            JsonSerializer js = new JsonSerializer();
+            List<Book> list=(List<Book>)js.Deserialize(jtr, typeof(List<Book>));
+
+            return list;
+        }
+
+        public static void Write(List<Book> an, StreamWriter sw)
+        {
+            JsonTextWriter jw = new JsonTextWriter(sw);
+
+            JsonSerializer js = new JsonSerializer();
+            js.Formatting = Formatting.Indented;
+            js.Serialize(jw, an);
+            jw.Close();
+        }
+    }
+}
---
+++ IBAtasks/DataAccess/task2/task2/XML.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace task2
+{
+    class XML
+    {
+        public static List<Book> Read(string fileName)
+        {
+            List<Book> list = new List<Book>();
+            string[] array = new string[4];
+            int kol;
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(fileName);
+            foreach (XmlNode node in xmlDoc.DocumentElement.ChildNodes)
+            {
+                kol = 0;
+
+                foreach (XmlNode item in node.ChildNodes)
+                {
+                    array[kol] = item.InnerText;
+                    kol++;
+                }
+
+                list.Add(new Book { Name = array[0], Autor = array[1], Description= array[2]});
+            }
+            return list;
+        }
+
+        public static void Write(List<Book> list, StreamWriter sw)
+        {
+            XmlSerializer formatter = new XmlSerializer(typeof(List<Book>));
+            using (TextWriter writerxml = sw)
+            {
+                formatter.Serialize(writerxml, list);
+            }
+        }
+
+
+    }
+}
