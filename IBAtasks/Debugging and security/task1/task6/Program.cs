24bb019165d484bfc09bfc16791ac33353ed430d
RomanVA97 <vasilje.roman2012@yandex.ru>
2018-02-13 13:00:09 +0300

    the program was created to encrypt data with rsa algorithm

---
+++ IBAtasks/Debugging and security/task1/task6/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool chek = true;
+            int k;
+            string str = "";
+            bool result = true;
+            while (chek)
+            {
+                Console.WriteLine("Что проверяем?");
+                Console.WriteLine("1-Email");
+                Console.WriteLine("2-URL");
+                Console.WriteLine("3-Paths");
+                Console.WriteLine("4-Price");
+                Console.WriteLine("0-Exit");
+                k = Convert.ToInt32(Console.ReadLine());
+                if (k == 0) break;
+                Console.Write("Input string: ");
+                str = Console.ReadLine();
+                switch(k)
+                {
+                    case 1:
+                        {
+                            result = Validate.Email(str);
+                        }
+                        break;
+                    case 2:
+                        {
+                            result = Validate.URL(str);
+                        }
+                        break;
+                    case 3:
+                        {
+                            result = Validate.Paths(str);
+                        }
+                        break;
+                    case 4:
+                        {
+                            result = Validate.Price(str);
+                        }
+                        break;
+                    case 0:
+                        {
+                            chek = false;
+                        }
+                        break;
+                }
+                
+                if (result)
+                {
+                    Console.WriteLine("Данные верны");
+                }
+                else
+                {
+                    Console.WriteLine("Данные неверны");
+                }
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+        }
+    }
+}
---
+++ IBAtasks/Debugging and security/task2/task2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+            string publicKeyXML = rsa.ToXmlString(false);
+            string privateKeyXML = rsa.ToXmlString(true);
+
+
+
+            UnicodeEncoding ByteConverter = new UnicodeEncoding();
+            Console.WriteLine("Введите строку для шифрования");
+            byte[] dataToEncrypt = ByteConverter.GetBytes(Console.ReadLine());
+
+            byte[] encryptedData;
+            using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+            {
+                RSA.FromXmlString(publicKeyXML);
+                encryptedData = RSA.Encrypt(dataToEncrypt, false);
+            }
+
+            byte[] decryptedData;
+            using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+            {
+                RSA.FromXmlString(privateKeyXML);
+                decryptedData = RSA.Decrypt(encryptedData, false);
+            }
+
+            string decryptedString = ByteConverter.GetString(decryptedData);
+            Console.WriteLine(decryptedString); // Displays: My Secret Data!
+            Console.ReadKey();
+        }
+    }
+}
