fbe36939c9e41dae49f23bcf21e60e30b5a7def7
RomanVA97 <vasilje.roman2012@yandex.ru>
2018-02-19 14:29:50 +0300

    modifed functional

--- IBAtasks/Debugging and security/task5/task5/Program.cs
+++ IBAtasks/Debugging and security/task5/task5/Program.cs
@@ -13,38 +13,47 @@ namespace task5
             bool chek = true;
             int k;
             List<double> list = new List<double>();
+            List<double> list2 = new List<double>();
             while (chek)
             {
                 Console.Clear();
+                Console.Write("Событие:");
                 foreach (double item in list) Console.Write(item + " ");
                 Console.WriteLine();
+                Console.Write("Вероятность:");
+                foreach (double item in list2) Console.Write(item + " ");
+                Console.WriteLine();
+                if (list2.Sum() > 1) Console.WriteLine("Сумма вероятностей не может быть больше 1");
+                Console.WriteLine("В массиве располагаются вероятности наступления событий и события");
+                Console.WriteLine();
                 Console.WriteLine("1 - Input");
-                Console.WriteLine("2 - Max");
-                Console.WriteLine("3 - Min");
-                Console.WriteLine("4 - Avg");
+                Console.WriteLine("2 - Выполнить действия");
                 Console.WriteLine("0 - Выход");
                 k = Convert.ToInt32(Console.ReadLine());
                 switch (k)
                 {
                     case 1:
                         {
-                            Console.WriteLine("Вводите");
-                            list.Add(Convert.ToInt32(Console.ReadLine()));
+                            Console.WriteLine("Событие ");
+                            list.Add(Convert.ToDouble(Console.ReadLine()));
+                            Console.WriteLine("Вероятность ");
+                            list2.Add(Convert.ToDouble(Console.ReadLine()));
                         }
                         break;
                     case 2:
                         {
-                            Console.WriteLine(list.Max());
-                        }
-                        break;
-                    case 3:
-                        {
-                            Console.WriteLine(list.Min());
-                        }
-                        break;
-                    case 4:
-                        {
-                            Console.WriteLine(list.Average());
+                            double result = 0;
+                            double M=0;
+                            double D = 0;
+                            for (int i = 0; i < list.Count; i++)
+                                M += list[i] * list2[i];
+                            Console.WriteLine("Матожидание - " + M);
+                            for (int i = 0; i < list.Count; i++)
+                                D += Math.Pow(list[i]-M, 2) * list2[i];
+                            Console.WriteLine("Дисперсия - " + D);
+                            Console.WriteLine("Стандартное отклонение - " + Math.Sqrt(D));
+
+
                         }
                         break;
                     case 0:
