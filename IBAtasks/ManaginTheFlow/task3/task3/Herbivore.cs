d52d65a69d7ada04a4cf626855df536a8fd3247b
RomanVA97 <vasilje.roman2012@yandex.ru>
2018-02-22 14:51:36 +0300

    added task3

---
+++ IBAtasks/ManaginTheFlow/task3/task3/Herbivore.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    class Herbivore
+    {
+
+        public Action OnChange { get; set; }
+        public void Raise()
+        {
+            OnChange?.Invoke();
+        }
+    }
+}
---
+++ IBAtasks/ManaginTheFlow/task3/task3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Plant plant = new Plant();
+            Carnivore carnivore = new Carnivore();
+            Herbivore herbivore = new Herbivore();
+            plant.OnChange += () => Console.WriteLine("Расту ради того чтобы скушать травоядное");
+            carnivore.OnChange += () => Console.WriteLine("Съешь меня");
+            herbivore.OnChange += () => Console.WriteLine("Ок, уже");
+            plant.Raise();
+            carnivore.Raise();
+            herbivore.Raise();
+
+            Console.ReadKey();
+        }
+    }
+}
